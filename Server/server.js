de23cce9eae5c1746e44aa9b9c8fa617c71a9c01
Ritz97 <ritam.de1997@gmail.com>
2018-09-10 17:59:04 +0530

    Create server.js

---
+++ Server/server.js
@@ -0,0 +1,35 @@
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+app.use(bodyParser.urlencoded({extended : false}));
+
+
+var server = app.listen(8081, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+   
+	console.log("Example app listening at http://%s:%s", host, port);
+});
+
+
+
+
+app.post('/', (req, res) => {
+	console.log(req.body.Body);
+
+	//Change the file directory as required
+	//"req.body.Body" should not be changed
+	fs.writeFile("C:\\Users\\HOME\\Desktop\\testFile.txt", req.body.Body, function(err) {
+  	if(err) {
+  		return console.log(err);
+  	}
+  	console.log("The file was saved");
+  })
+
+
+	res.send("Received");
+});
+
