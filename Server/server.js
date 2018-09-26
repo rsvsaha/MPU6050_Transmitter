5b833362348e1783f29a5043d8f06324677ff27f
Ritz97 <ritam.de1997@gmail.com>
2018-09-26 20:23:33 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
@@ -15,21 +13,32 @@ var server = app.listen(8081, function () {
 });
 
 
-
-
 app.post('/', (req, res) => {
 	console.log(req.body.Body);
 
 	//Change the file directory as required
 	//"req.body.Body" should not be changed
-	fs.writeFile("C:\\Users\\HOME\\Desktop\\testFileNEW.txt", req.body.Body, 'utf8', function(err) {
-  	if(err) {
-  		return console.log(err);
-  	}
-  	console.log("The file was saved");
-  })
+	fs.writeFile('C:\\Users\\HOME\\Desktop\\testFileNEW.txt', req.body.Body, 'utf8', function(err) {
+  		if(err) {
+  			return console.log(err);
+  		}
+    })
+	res.send("Received");
+});
 
 
+app.post('/train', (req, res) => {
+	console.log(req.body.Body);
+
+	//Change the file directory as required
+	//"req.body.Body" should not be changed
+	//"req.body.Name" should not be changed
+	var filename = req.body.Name + '.txt';
+	fs.writeFile('C:\\Users\\HOME\\Desktop\\' + filename, req.body.Body, 'utf8', function(err) {
+  		if(err) {
+  			return console.log(err);
+  		}
+    })
 	res.send("Received");
 });
 
