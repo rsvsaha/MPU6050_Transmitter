4e4e734b8a525fff54db039da96804d69ede547d
rkbasu02 <40673142+rkbasu02@users.noreply.github.com>
2018-11-04 16:46:35 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,36 @@
+
+
+import os,process
+import numpy as np
+
+def create():
+    x_data=[]
+    y_data=[]
+    count=0
+    d={}
+    root="Samples"
+    
+    
+    for path,subdirs,files in os.walk(root):
+        for name in files:
+            filename=os.path.join(path,name)
+            #print(filename)
+            
+            Samp=process.Sample.load_from_file(filename)
+            Sample_after_concat=np.concatenate((Samp.accx,Samp.accy,Samp.accz,Samp.gyx,Samp.gyy,Samp.gyz),axis=0)
+            
+            x_data.append(Sample_after_concat)
+            
+            reqd_name=name.split('_')[0]
+            
+            if reqd_name not in d.keys():
+                d[reqd_name]=count
+                count=count+1
+                
+            y_data.append(d[reqd_name])
+        
+    #print(d)
+    #print(len(x_data[0]))   
+    #print(len(y_data))         
+
+create() 
